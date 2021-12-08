c9f1a95f78fea93a7d0e09411610ebe791fd03c7
unknown <nguyendatrt@gmail.com>
2021-12-09 02:37:36 +0700

    update

---
+++ src/app/controllers/NewsControllers.js
@@ -0,0 +1,15 @@
+class NewController {
+
+    // [get] /new
+    index(req, res) {
+        res.render('news');
+    }
+
+    // [get] /new/:slug
+    show(req, res) {
+        res.send('New detail!!!');
+    }
+
+}
+
+module.exports = new NewController;
---
+++ src/app/controllers/SiteControllers.js
@@ -0,0 +1,16 @@
+class SiteController {
+
+    // [get] /home
+    index(req, res) {
+        res.render('home');
+    }
+
+
+    // [get] /search
+    search(req, res) {
+        res.render('search');
+    }
+
+}
+
+module.exports = new SiteController;
